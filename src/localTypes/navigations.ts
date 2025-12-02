f33b965d05f6304b498f0d2747744f47606b94c7
typescreep <typescreep@prorobotech.ru>
2025-12-02 15:21:39 +0300

    navigation alias paths

--- src/localTypes/navigations.ts
+++ src/localTypes/navigations.ts
@@ -9,15 +9,18 @@ export type TNavigationResource = unknown & {
     projects?: {
       clear: string
       change: string
+      aliasPath?: string
     }
     instances?: {
       clear: string
       change: string
       mapOptionsPattern?: string
+      aliasPath?: string
     }
     namespaces?: {
       clear: string
       change: string
+      aliasPath?: string
     }
     baseFactoriesMapping?: Record<string, string>
   }
